8a7ba53ca59030f1d4202baef186983aed20f3ce
iamanggi <anggipuspita452@gmail.com>
2025-01-10 14:45:53 +0700

    Menambahkan data class MahasiswaEvent dan fungsi konversi ke model Mahasiswa

---
+++ app/src/main/java/com/example/praktikum10/ui/ViewModel/InsertViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.praktikum10.ui.ViewModel
+
+import com.example.praktikum10.model.Mahasiswa
+
+// data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+//menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
